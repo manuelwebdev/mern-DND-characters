d714c080281c4ace245c2fe14782e02288827159
manuelwebdev <mfewcar@gmail.com>
2023-11-14 18:28:47 -0700

    Trying to configure some tailwind styling.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,15 +4,35 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <>
-      <header>
-        <div className='bg-white'>
-          <Link to='/'>
-            <h1 className='text-3xl font-bold'>MERN D&D</h1>
-          </Link>
+    <nav className='p-4'>
+      <div className='container mx-auto'>
+        <div className='flex items-center justify-between'>
+          <div className=' font-bold text-xl'>My Website</div>
+          <ul className='flex space-x-4'>
+            <li>
+              <a href='#' className=' hover:text-gray-600'>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href='#' className=' hover:text-gray-600'>
+                About
+              </a>
+            </li>
+            <li>
+              <a href='#' className=' hover:text-gray-600'>
+                Services
+              </a>
+            </li>
+            <li>
+              <a href='#' className=' hover:text-gray-600'>
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-      </header>
-    </>
+      </div>
+    </nav>
   )
 }
 
