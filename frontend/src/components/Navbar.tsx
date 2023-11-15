b84f7b8a0ddef9a19a4dbd03f424485505ee3e28
manuelwebdev <mfewcar@gmail.com>
2023-11-14 19:32:30 -0700

    cleaning up html

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,35 +4,13 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='p-4'>
+    <header className='py-4'>
       <div className='container mx-auto'>
-        <div className='flex items-center justify-between'>
-          <div className=' font-bold text-xl'>My Website</div>
-          <ul className='flex space-x-4'>
-            <li>
-              <a href='#' className=' hover:text-gray-600'>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#' className=' hover:text-gray-600'>
-                About
-              </a>
-            </li>
-            <li>
-              <a href='#' className=' hover:text-gray-600'>
-                Services
-              </a>
-            </li>
-            <li>
-              <a href='#' className=' hover:text-gray-600'>
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
+        <Link to={'/'} className=' font-bold text-xl'>
+          My Website
+        </Link>
       </div>
-    </nav>
+    </header>
   )
 }
 
