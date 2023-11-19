e5a1d441fb2235a8c8b885e3f93b5450fddda539
manuelwebdev <mfewcar@gmail.com>
2023-11-18 20:03:25 -0700

    Working on displaying character pages.

--- frontend/src/components/CharacterDetails.tsx
+++ frontend/src/components/CharacterDetails.tsx
@@ -17,6 +17,25 @@ export default function CharacterDetails(props: Props) {
       createdAt = new Date(),
     },
   } = props
+
+  const handleActions = (e: any) => {}
+
+  const handleDelete = async (e: any) => {
+    e.preventDefault()
+    try {
+      const res = await fetch(`http://localhost:3000/api/characters/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      if (res.ok) {
+        console.log('deleted')
+      }
+    } catch (error) {
+      console.warn(error)
+    }
+  }
   return (
     <Link
       to={`/characters/${id}`}
@@ -30,21 +49,25 @@ export default function CharacterDetails(props: Props) {
         bg-slate-100 
         min-w-fit 
         grid 
-        grid-cols-5 
-        auto-rows-min	
+        grid-cols-10         
         '
     >
-      <h2 className='uppercase text-xl text-emerald-600 col-span-3'>
-        {`${name} the ${background || 'adventurer'}`}
-      </h2>
-      <p className='col-span-2 justify-self-end'>
-        lvl:{level} {characterClass}
-      </p>
-      <p className='text-slate-500 text-md col-span-5'>{race}</p>
-      <p className='text-slate-800 text-lg col-span-2'>{alignment}</p>
-      <p className='text-slate-500 text-sm col-span-3 self-end'>
-        Created at: {new Date(createdAt).toDateString()}
-      </p>
+      <div className='grid grid-cols-5 auto-rows-min	 col-span-9'>
+        <h2 className='uppercase text-xl text-emerald-600 col-span-3'>
+          {`${name} the ${background || 'adventurer'}`}
+        </h2>
+        <p className='col-span-2 justify-self-end'>
+          lvl:{level} {characterClass}
+        </p>
+        <p className='text-slate-500 text-md col-span-5'>{race}</p>
+        <p className='text-slate-800 text-lg col-span-2'>{alignment}</p>
+        <p className='text-slate-500 text-sm col-span-3 self-end justify-self-end'>
+          Created at: {new Date(createdAt).toDateString()}
+        </p>
+      </div>
+      <button className='container col-span-1' onClick={handleActions}>
+        ...
+      </button>
     </Link>
   )
 }
--- frontend/src/pages/CharacterView.tsx
+++ frontend/src/pages/CharacterView.tsx
@@ -1,50 +1,49 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-type Props = {}
+type Character = {
+  name: string
+  class: string
+  level: number
+  race: string
+  background: string
+  alignment: string
+  createdAt: Date
+}
 
-export default function CharacterView({}: Props) {
-  const [character, setCharacter] = useState<any>({
-    name: '',
-    class: '',
-    level: 0,
-    race: '',
-    background: '',
-    alignment: '',
-    createdAt: new Date(),
-  })
-  const { id } = useParams()
+export default function CharacterView() {
+  const [character, setCharacter] = useState<Character | null>(null)
+  const { id } = useParams<{ id: string }>()
 
   useEffect(() => {
-    console.log(id)
-    ;(async () => {
+    const fetchCharacter = async () => {
       try {
-        const res = await fetch(`http://localhost:3000/api/characters/${id}`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-        console.log(res)
-        if (res.ok) {
-          const data = await res.json()
-          setCharacter(data)
-        }
+        const response = await fetch(
+          `http://localhost:3000/api/characters/${id}`
+        )
+        const data = await response.json()
+        setCharacter(data)
       } catch (error) {
-        console.warn(error)
+        console.error('Error fetching character:', error)
       }
-    })()
-  }, [])
+    }
+
+    fetchCharacter()
+  }, [id])
+
+  if (!character) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div>
-      <h1>{character?.name}</h1>
-      <p>{character?.class}</p>
-      <p>{character?.level}</p>
-      <p>{character?.race}</p>
-      <p>{character?.background}</p>
-      <p>{character?.alignment}</p>
-      <p>{character?.createdAt}</p>
+      <h1>{character.name}</h1>
+      <p>Class: {character.class}</p>
+      <p>Level: {character.level}</p>
+      <p>Race: {character.race}</p>
+      <p>Background: {character.background}</p>
+      <p>Alignment: {character.alignment}</p>
+      <p>Created At: {character.createdAt.toISOString()}</p>
     </div>
   )
 }
